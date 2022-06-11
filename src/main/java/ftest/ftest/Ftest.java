3ff133ec2b0c9d8fb8f0e0a777392dcb322c488c
Foxiel <105858467+Foxiel@users.noreply.github.com>
2022-06-12 00:02:02 +0200

    Initial commit

---
+++ src/main/java/ftest/ftest/Ftest.java
@@ -0,0 +1,20 @@
+package ftest.ftest;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Ftest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getLogger().info("Hello World");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        Bukkit.getLogger().info("Hello World");
+    }
+}
